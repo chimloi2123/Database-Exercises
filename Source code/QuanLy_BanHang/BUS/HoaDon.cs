6d8d67ee084cdc48db9d1c4b608037a975bb3fac
Truong Thao <thaotruonggl30@gmail.com>
2021-11-02 00:00:17 +0700

    Revert "update giao dien"
    This reverts commit 95570a5635cfc0042aee791f8dd20f992bd63e45.

---
+++ Source code/QuanLy_BanHang/BUS/HoaDon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAO;
+using DTO;
+namespace BUS
+{
+    public class HoaDon
+    {
+        public static List<DTO_HoaDon> getDSHoaDon()
+        {
+            return DB_HoaDon.getDSHoaDon();
+        }
+    }
+}
